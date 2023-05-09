78f003f3b1bdc8768708eab9860c8e2ecd7b4c33
Domengo <dominicsengo@gmail.com>
2023-05-10 02:52:00 +0300

    search

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" 11-main """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """find topic"""
+    return [school for school in mongo_collection.find_one({"topic": topic})]
