01c9d9a9ccd7f3adb0690a914a784fd39d0ff195
Domengo <dominicsengo@gmail.com>
2023-05-10 02:19:23 +0300

    list all documents in python

---
+++ 0x01-NoSQL/oo.py
@@ -0,0 +1,26 @@
+from pymongo import MongoClient
+
+# connect to MongoDB
+client = MongoClient()
+db = client.logs
+collection = db.nginx
+
+# count total number of logs
+total_logs = collection.count_documents({})
+
+# count number of logs with each HTTP method
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {}
+for method in http_methods:
+    count = collection.count_documents({"method": method})
+    method_counts[method] = count
+
+# count number of logs with method=GET and path=/status
+status_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+# print stats
+print(f"{total_logs} logs")
+print("Methods:")
+for method, count in method_counts.items():
+    print(f"\t{method}: {count}")
+print(f"{status_count} logs with method=GET and path=/status")
