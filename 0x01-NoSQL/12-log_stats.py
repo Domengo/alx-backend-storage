d653eb0826e042bdc35853e2990f13f6d69f32f2
Domengo <dominicsengo@gmail.com>
2023-05-10 03:14:57 +0300

    log stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,21 +8,23 @@ db = client.logs
 collection = db.nginx
 
 # count total number of logs
-total_logs = collection.count_documents({})
+if __name__ == '__main__':
+    total_logs = collection.count_documents({})
+    # count number of logs with each HTTP method
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {}
+    for method in http_methods:
+        count = collection.count_documents({"method": method})
+        method_counts[method] = count
+    # count number of logs with method=GET and path=/status
+    status_count = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
 
-# count number of logs with each HTTP method
-http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-method_counts = {}
-for method in http_methods:
-    count = collection.count_documents({"method": method})
-    method_counts[method] = count
-
-# count number of logs with method=GET and path=/status
-status_count = collection.count_documents({"method": "GET", "path": "/status"})
-
-# print stats
-print(f"{total_logs} logs")
-print("Methods:")
-for method, count in method_counts.items():
-    print(f"\t{method}: {count}")
-print(f"{status_count} status check")
+    # print stats
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method, count in method_counts.items():
+        print(f"\t{method}: {count}")
+    print(f"{status_count} status check")
