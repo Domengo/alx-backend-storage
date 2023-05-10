01454857a79f49e1139b467ba9fa219460caf467
Domengo <dominicsengo@gmail.com>
2023-05-10 03:37:41 +0300

    top student

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -4,22 +4,10 @@
 
 def top_students(mongo_collection):
     """return top student"""
-    pipeline = [
-        {
-            '$project': {
-                '_id': 1,
-                'name': 1,
-                'averageScore': {
-                    '$avg': {
-                        'avg': '$topics.score'}
-                },
-                'topics': 1
-            }
-        },
-        {
-            '$sort': {
-                'averageScore': -1
-            }
-        }
-    ]
+    pipeline = [{"$project": {"_id": 1, "name": 1,
+                            "averageScore": {
+                                "$avg": {
+                                    "$avg": "$topics.score", },
+                                }, "topics": 1, }, },
+                {"$sort": {"averageScore": -1}, }, ]
     return list(mongo_collection.aggregate(pipeline))
