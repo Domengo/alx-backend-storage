6273ec9265c05b835fc7b394b5c5db2221bfa0c2
Domengo <dominicsengo@gmail.com>
2023-05-11 03:37:13 +0300

    implementing expiring web cache

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 """Implementing an expiring web cache and tracker"""
+import redis
 import requests
+from functools import wraps
+from typing import Callable
+
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
 def wrapper(method: callable) -> callable:
     """wrapper"""
-    def wrapped(url):
+    @wraps
+    def wrapped(url) -> str:
         """wrapped"""
-        return method(url)
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
     return wrapped
 
 
